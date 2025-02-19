d69d34b5241a6d3fa05ecceddaf79f2b8680c37d
DJCoggs2005 <dakota.coughlin@bsu.edu>
2025-02-18 23:37:10 -0500

    Added sounds
    Accept and Deny sounds, and timer before switching to next candidate

--- jobhiringsim/mainscene.gd
+++ jobhiringsim/mainscene.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var resume_node = get_node("randomizedResume")
 @onready var job_node = get_node("randomizedJob")
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,8 +15,14 @@ func _process(delta: float) -> void:
 
 
 func _on_accept_button_pressed() -> void:
+	$PlayAccept.play()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().reload_current_scene() 
+	
 
 
 func _on_deny_button_pressed() -> void:
+	$PlayDeny.play()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().reload_current_scene() 
+	
